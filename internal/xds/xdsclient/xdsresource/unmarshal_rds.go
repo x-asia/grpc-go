f54e2ce15bc89a5c3e44743a5ecd0d1aaa05912a
Yuki Ito <mrno110y@gmail.com>
2026-01-30 11:19:47 +0900

    debug: xds

--- internal/xds/xdsclient/xdsresource/unmarshal_rds.go
+++ internal/xds/xdsclient/xdsresource/unmarshal_rds.go
@@ -307,6 +307,7 @@ func routesProtoToSlice(routes []*v3routepb.Route, csps map[string]clusterspecif
 			fmt.Printf("config: %v, ops: %v, action AutoHostRewrite: %+v\n", envconfig.XDSAuthorityRewrite, opts.ServerConfig.SupportsServerFeature(xdsclient.ServerFeatureTrustedXDSServer), action.GetAutoHostRewrite())
 			if envconfig.XDSAuthorityRewrite {
 				if opts != nil && opts.ServerConfig != nil && opts.ServerConfig.SupportsServerFeature(xdsclient.ServerFeatureTrustedXDSServer) {
+					fmt.Printf("action.GetAutoHostRewrite: %t\n", action.GetAutoHostRewrite().GetValue())
 					route.AutoHostRewrite = action.GetAutoHostRewrite().GetValue()
 				}
 			}
